0eeda5239ce6872017802789a74b56a20ceaec7d
franco <mancillafranco71@gmail.com>
2026-07-03 18:35:58 -0300

    pago controller

--- src/modules/interfaces/pago.controller.ts
+++ src/modules/interfaces/pago.controller.ts
@@ -7,29 +7,28 @@ const repositoryPago = new PrismaPagoRepository()
 const repositoryVenta = new PrismaVentaRepository()
 const service = new PagoService(repositoryPago,repositoryVenta)
 
-export const pagar = async (
-    req:Request,
-    res:Response
-) => {
-    try{
+export const pagar = async (req: Request, res: Response) => {
+    try {
 
-        const id = Number(req.params.id)
+        const id = Number(req.params.id);
+
         if (isNaN(id)) {
-             throw new Error("venta no puede ser nula");   
+            throw new Error("venta no puede ser nula");
         }
 
-        const { pagos } = req.body;
+        const pagos = req.body.pagos;
 
         if (!pagos) {
-             throw new Error("pago no puede ser nulo");
+            throw new Error("Los pagos no pueden ser nulos");
         }
-        const result = await service.crearPago(id,pagos)
-        return res.json(result)
 
+        const result = await service.crearPago(id, pagos);
 
-    }catch(error:any){
-        if (error) {
-            return res.status(400).json({message:error.message})
-        }
+        return res.json(result);
+
+    } catch (error: any) {
+        return res.status(400).json({
+            message: error.message
+        });
     }
-}
+};
